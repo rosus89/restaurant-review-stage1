2226ce6b9a6e96a3dc77fd5ee3e558c8d61a1e5c
Stefan Rosu <rosus89@gmail.com>
2018-12-29 14:32:45 +0000

    Fix: Service Worker + removed github path

--- sw.js
+++ sw.js
@@ -1,47 +1,46 @@
 // Source: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 
+let cache_name = 'restaurants-review-Cache-v1';
+let cached_items = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/css/styles.css',
+    '/data/restaurants.json'
+    
+]
 // Install
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('restaurants-review-Cache-v1').then(function (cache) {
+        caches.open(cache_name).then(function (cache) {
             console.log('Opened cache');
-            return cache.addAll(
-                [   '/restaurant-review-stage1/',
-                    '/restaurant-review-stage1/img/1.jpg',
-                    '/restaurant-review-stage1/img/2.jpg',
-                    '/restaurant-review-stage1/img/3.jpg',
-                    '/restaurant-review-stage1/img/4.jpg',
-                    '/restaurant-review-stage1/img/5.jpg',
-                    '/restaurant-review-stage1/img/6.jpg',
-                    '/restaurant-review-stage1/img/7.jpg',
-                    '/restaurant-review-stage1/img/8.jpg',
-                    '/restaurant-review-stage1/img/9.jpg',
-                    '/restaurant-review-stage1/img/10.jpg',
-                    '/restaurant-review-stage1/js/dbhelper.js',
-                    '/restaurant-review-stage1/js/main.js',
-                    '/restaurant-review-stage1/js/restaurant_info.js',
-                    '/restaurant-review-stage1/css/styles.css'
-                ]
-            );
+            return cache.addAll(cached_items);
         })
     );
 });
 
-// Cache falling back to the network offline first method
+// activate
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
 
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then((response) => {
-//             console.log('fetch request');
-//             return response || fetch(event.request);
-//         })
-//     ); 
-// });
-
+// Fetch
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.open('restaurants-review-Cache-v1').then(function (cache) {
+        caches.open(cache_name).then(function (cache) {
             return cache.match(event.request).then(function (response) {
                 return response || fetch(event.request).then(function (response) {
                     cache.put(event.request, response.clone());
